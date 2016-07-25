e6af92b8e2439133b06cdaa540ef9e950b9bc199
Anatoly Galiulin <galiulin.anatoly@gmail.com>
2016-07-25 18:13:35 +0700

    WIP

--- tests/chrome_src/main.nim
+++ tests/chrome_src/main.nim
@@ -1,3 +1,26 @@
-{.emit: """
-console.log("Hello, world!")
+type
+  Port {.importc.} = object
+
+proc connectPort(name: cstring): Port =
+  {.emit: """
+  `result` = chrome.runtime.connectNative(`name`);
 """.}
+
+proc onMessage(p: Port, cb: proc (rsp: cstring) {.noconv.}) =
+  {.emit: """
+  `p`.onMessage.addListener(`cb`);
+""".}
+
+proc postMessage(p: Port, msg: cstring) =
+  {.emit: """
+  `p`.postMessage(`msg`);
+""".}
+var p = connectPort("org.acme.test_host")
+
+p.onMessage(proc (msg: cstring) {.noconv.} =
+  {.emit: """
+  console.log("Received message: " + `msg`);
+""".}
+)
+
+p.postMessage("Hello, world!")
--- tests/host/main.nim
+++ tests/host/main.nim
@@ -1,7 +1,38 @@
-import os
+import os, logging, future
+
+var fl = newFileLogger(getAppDir() / "host.log", fmtStr = "$datetime [$levelname]: ")
+logging.addHandler(fl)
+
+proc readMessage: string =
+  var size = 0'i32
+  var read = stdin.readBuffer(addr size, sizeof(size))
+  assert(read == sizeof(size))
+  var buff = newSeq[char](size.int + 1)
+  read = 0
+  while read != size:
+    let r = stdin.readBuffer(addr buff[read], size - read)
+    assert(r != 0)
+    read += r
+  buff[size] = '\0'
+  result = $cast[cstring](addr buff[0])
+
+proc writeMessage(msg: string) =
+  var size = msg.len.int32
+  var written = stdout.writeBuffer(addr size, sizeof(size))
+  assert(written == sizeof(size))
+  written = stdout.writeBuffer(msg.cstring, size)
+  assert(written == size)
 
 proc main =
-  echo "Hello, world!"
+  info "Host process is running"
+  addQuitProc(proc() {.noconv.} =
+                info "Shutdown host process"
+  )
+
+  while true:
+    let msg = readMessage()
+    info("Received: " & msg)
+    writeMessage(msg)
 
 when isMainModule:
   main()
